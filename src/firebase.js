c2b65fd5d84df6d87b60b195a22f7aec2b23f18d
Yxychris <yxychris@outlook.com>
2021-02-27 16:58:40 -0500

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDguRKDDW5EmsMP4CL95T3hkpk8YZfaTk8",
+    authDomain: "twitter-clone-3dd8a.firebaseapp.com",
+    projectId: "twitter-clone-3dd8a",
+    storageBucket: "twitter-clone-3dd8a.appspot.com",
+    messagingSenderId: "21557893029",
+    appId: "1:21557893029:web:27f086d84b88027a463a4c",
+    measurementId: "G-GT9XBXN1D4"
+  };
